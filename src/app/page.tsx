fd5e838a33cf7641c2f365acb6da63306a34fecd
Kerry Creech <kerrywcreech@gmail.com>
2024-07-12 21:06:30 -0400

    make app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,58 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col gap-y-4 p-24">
+      <h1><b>Colonial Period and Early Independence (1492-1959)</b></h1>
+      <p>- 1492: Christopher Columbus arrives in Cuba, claiming the island for Spain.<br></br>- 1511: Spanish colonization begins, leading to centuries of Spanish rule.<br></br>
+- 1868-1878: Ten Years' War, the first major attempt for independence from Spain.
+<br></br>- 1895-1898: Cuban War of Independence, leading to the end of Spanish rule.
+<br></br>- 1898: Spanish-American War; Cuba becomes a U.S. protectorate.
+<br></br>- 1902: Establishment of the Republic of Cuba; the U.S. retains significant influence.
+<br></br></p>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+<h1><b>Pre-Revolutionary Period (1902-1959)</b></h1>
+      <p>- 1902-1933: Several presidencies marked by political instability and U.S. intervention.
+<br></br>- 1933: Sergeants' Revolt leads to the overthrow of President Gerardo Machado.
+<br></br>- 1940: A new constitution is adopted under President Fulgencio Batista, introducing progressive reforms.
+<br></br>- 1952: Batista leads a coup, becoming a dictator and suspending the 1940 constitution.
+<br></br></p>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+<h1><b>Revolutionary Period (1953-1959)</b></h1>
+      <p>- 1953: Fidel Castro leads the failed attack on Moncada Barracks, marking the start of the Cuban Revolution.
+<br></br>- 1956: Castro and his followers land in Cuba on the Granma and start the guerrilla war.<br></br>
+- 1959: Batista flees Cuba; Fidel Castro’s forces take control, marking the revolution's victory.
+<br></br></p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+<h1><b>Post-Revolutionary Period (1959-Present)</b></h1>
+      <p><b>1960s</b><br></br>
+        - 1960: Nationalization of U.S.-owned businesses and land reforms.<br></br>
+- 1961: U.S. breaks diplomatic relations; Bay of Pigs invasion fails.<br></br>
+- 1962: Cuban Missile Crisis; U.S. imposes a trade embargo.<br></br>
+- 1965: Communist Party of Cuba established.<br></br>
+<b>1970s</b><br></br>
+- 1976: New socialist constitution centralizes power.<br></br>
+- 1975-1990: Alignment with the Soviet Union, receiving economic and military aid.<br></br>
+<b>1980s</b><br></br>
+- 1980: Mariel Boatlift leads to mass emigration.<br></br>
+- 1986: Rectification Campaign reverses market-oriented reforms.<br></br>
+<b>1990s</b><br></br>
+- 1991: Soviet Union collapses, leading to the Special Period economic crisis.<br></br>
+- 1994: Balseros crisis results in another emigration wave.<br></br>
+<b>2000s</b><br></br>
+- 2000: Elian Gonzalez affair strains U.S.-Cuba relations.<br></br>
+- 2002: U.S. tightens embargo under Helms-Burton Act.<br></br>
+- 2006: Fidel Castro cedes power to Raúl Castro due to health issues.<br></br>
+- 2008: Raúl Castro becomes President, initiating economic reforms.<br></br>
+<b>2010s</b><br></br>
+- 2011: Significant economic reforms approved by the Communist Party Congress.<br></br>
+- 2014: Announcement of normalization of relations with the U.S.<br></br>
+- 2015: Reopening of U.S. and Cuban embassies.<br></br>
+- 2016: Death of Fidel Castro.<br></br>
+- 2018: Miguel Díaz-Canel becomes President, ending the Castro era.<br></br>
+<b>2020s</b><br></br>
+- 2021: Protests over economic conditions and COVID-19 response.<br></br>
+- 2022: Continued economic reforms and attempts to attract foreign investment.<br></br>
+- 2023: New family code approved, expanding rights for same-sex couples.</p>
     </main>
   );
 }
